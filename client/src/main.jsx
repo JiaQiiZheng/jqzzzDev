0a3a8f0d16afed0c168f2615c5e9d6d2927c9a0c
Jiaqi <dxq7nz@virginia.edu>
2023-01-24 01:47:16 -0500

    vercel

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDom from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
