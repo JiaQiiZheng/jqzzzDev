f6247dfccc0e9548eb291489098ce4f46a6866fd
Jiaqi <dxq7nz@virginia.edu>
2023-01-25 00:20:36 -0500

    serverTestFinal

--- server/routes/home.js
+++ server/routes/home.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  return res.json({ message: "Hello From Express!" });
+  return res.send("Hello From Express!");
 });
 
 export default router;
